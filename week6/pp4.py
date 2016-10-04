fb1b2fb54f550095e8ca50da3ab02561630b57c4
Stanley Lio <stanleylio@gmail.com>
2016-10-03 16:34:26 -1000

    .

--- week6/pp4.py
+++ week6/pp4.py
@@ -1,20 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import sys
+sys.path.append('../week3')
+import ads1115
 
 fig, ax = plt.subplots()
 line, = ax.plot(np.random.rand(10))
 ax.set_ylim(0, 1)
 
 
+sensor = ads1115.ADS1115()
+
+D = []
 def update(data):
-    line.set_ydata(data)
+    print data
+    D.append(data)
+    while len(D) > 100:
+        D.pop(0)
+    line.set_xdata(range(0,len(D)))
+    line.set_ydata(D)
     return line,
 
 
 def data_gen():
-    while True:
-        yield np.random.rand(10)
+    #while True:
+        #yield np.random.rand(10)
+    yield sensor.read_adc(0)
 
 ani = animation.FuncAnimation(fig, update, data_gen, interval=100)
 plt.show()
