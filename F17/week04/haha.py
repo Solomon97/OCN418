578b26754d013cbb4859e566bc95a2b64b26dc5d
Stanley H I Lio <stanleylio@users.noreply.github.com>
2017-09-18 21:35:50 -1000

    .

--- F17/week04/haha.py
+++ F17/week04/haha.py
@@ -1,4 +1,3 @@
-##testing lcd fun in class
 import sys,time
 sys.path.append('/home/pi/Desktop/OCN418/F17/week04')
 from lcd20x4 import LCD
@@ -8,7 +7,7 @@ lcd = LCD(bus=1,address=0x3f)
 
 line = 'should we gather for whisky and cigar tonight?'
 
-#lcd.write_lines(['cast','into','the bowel','of the earth'])
+#lcd.write_lines(['cast','into','the bowels','of the earth'])
 
 i = 0
 
@@ -16,4 +15,4 @@ while True:
     lcd.write_lines([line[i:i+20],'','',''])
     time.sleep(1)
     i = i + 1
-    i = i % 20
+    i = i % len(line)
